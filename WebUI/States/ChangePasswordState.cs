b882f88beba1b25722414a336a015c7c97cb15bc
TheSoftNode <thesoftnode@gmail.com>
2024-07-09 14:43:32 +0100

    Manages the user, Admin and orders states

---
+++ WebUI/States/ChangePasswordState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WebUI.States;
+
+public class ChangePasswordState
+{
+	public string? PasswordType = "password";
+	public bool PasswordState = true;
+	public string DisplayText = "Show";
+	public Action? Changed;
+
+	public void ChangePasswordType()
+	{
+		PasswordState = !PasswordState;
+		if (!PasswordState)
+		{
+			PasswordType = "text";
+			DisplayText = "Hide";
+		}
+		else
+		{
+			PasswordType = "password";
+			DisplayText = "show";
+		}
+		Changed?.Invoke();
+	}
+}
---
+++ WebUI/States/User/UserActiveOrderCountState.cs
@@ -0,0 +1,28 @@
+﻿using Application.Service.Orders.Queries;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace WebUI.States.User;
+
+public class UserActiveOrderCountState(IServiceProvider serviceProvider)
+{
+	public int ProcessingCount { get; set; }
+	public int DeliveringCount { get; set; }
+	public int DeliveredCount { get; set; }
+	public int CanceledCount { get; set; }
+	public event Action? StateChanged;
+
+	public async Task GetActiveOrdersCount(string userId)
+	{
+		using var scope = serviceProvider.CreateScope();
+		var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+		var response = (await mediator.Send(new GetGenericOrdersCountQuery(userId, false)));
+		ProcessingCount = response.Processing;
+		DeliveringCount = response.Delivering;
+		DeliveredCount = response.Delivered;
+		CanceledCount = response.Canceled;
+		StateChanged?.Invoke();
+	}
+}
