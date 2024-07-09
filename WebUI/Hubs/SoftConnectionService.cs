5530366a2ef7f8bacc3e513d929ca5d70cc36e43
TheSoftNode <thesoftnode@gmail.com>
2024-07-09 14:42:54 +0100

    Works with Signal R for real time tracking

---
+++ WebUI/Hubs/SoftConnectionService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace WebUI.Hubs;
+
+public class SoftHubConnectionService
+{
+	private readonly HubConnection _hubConnection;
+
+	public SoftHubConnectionService(NavigationManager navigationManager)
+	{
+		_hubConnection = new HubConnectionBuilder()
+			.WithUrl(navigationManager.ToAbsoluteUri("/communicationhub"))
+			.Build();
+
+		_hubConnection.StartAsync();
+	}
+
+	public HubConnection GetHubConnection() => _hubConnection;
+}
