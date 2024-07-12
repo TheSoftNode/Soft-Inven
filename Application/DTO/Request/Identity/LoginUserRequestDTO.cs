c880c304c916a1e4c3f89180a157afc56da8b30a
TheSoftNode <thesoftnode@gmail.com>
2024-07-12 12:36:05 +0100

    Add better validation message to the password field

--- Application/DTO/Request/Identity/LoginUserRequestDTO.cs
+++ Application/DTO/Request/Identity/LoginUserRequestDTO.cs
@@ -10,7 +10,7 @@ public class LoginUserRequestDTO
     public string Email { get; set; }
 
     [Required]
-    [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$", ErrorMessage = "Invalid Password")]
+    [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$", ErrorMessage = "Password must contain at least a Capital letter, a number, a special character and a small letter alphabet")]
     [MinLength(8), MaxLength(100)]
     public string Password { get; set; }
 }
